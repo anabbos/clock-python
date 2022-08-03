bd060dbf987feee52324ff4a0b5d81685e4db76d
theabbos <abbosdevv@gmail.com>
2022-08-03 10:17:57 +0500

    the end project

--- clock.py
+++ clock.py
@@ -1,6 +1,18 @@
 from tkinter import *
 from time import *
 
+def update():
+    time_string = strftime("%I:%M:%S %p")
+    time_label.config(text=time_string)
+
+    day_string = strftime(%A)
+    day_label.config(text=day_string)
+
+    date_string = strftime("%B %d, %Y")
+    date_label.config(date_string)
+
+    window.after(1000,update)
+
 window.Tk()
 window.title("Clock")
 
@@ -12,5 +24,7 @@ day_label.pack()
 
 date_label = Label(window,font=("Ink Free", 30))
 date_label.pack()
+
+update()
 
 window.mainloop()
