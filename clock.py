3e9989177084534629cc046f5f40a140a0de457a
theabbos <abbosdevv@gmail.com>
2022-08-03 10:13:51 +0500

    created GUI window

--- clock.py
+++ clock.py
@@ -1,2 +1,16 @@
 from tkinter import *
 from time import *
+
+window.Tk()
+window.title("Clock")
+
+time_label = Label(window,font=("Arial",50),fg="#00FF00", bg="black")
+time_label.pack()
+
+day_label = Label(window,font=("Ink Free",25))
+day_label.pack()
+
+date_label = Label(window,font=("Ink Free", 30))
+date_label.pack()
+
+window.mainloop()
